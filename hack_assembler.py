1a60a05c744f7878a99fad2ae9d3d8cf48b8668f
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2023-04-29 14:42:56 -0500

    Add initialize_argparser, add tests

--- hack_assembler.py
+++ hack_assembler.py
@@ -1,9 +1,10 @@
 from argparse import ArgumentParser, Namespace
+import sys
 
 from hasm_parser import Parser
 
 
-def initialize_arguments() -> Namespace:
+def initialize_argparser() -> ArgumentParser:
     """
     Initialize the ArgumentParser for command-line-arguments
     """
@@ -12,13 +13,27 @@ def initialize_arguments() -> Namespace:
         prog="HackAssembler", description="Assemble Hack .asm file into machine code."
     )
     arg_parser.add_argument(
-        "file", type=str, help="absolute filepath to the .asm file to be assembled"
+        "file", metavar="file.asm", type=str, help="absolute filepath of the .asm file to be assembled"
     )
 
-    return arg_parser.parse_args()
+    return arg_parser
+
+
+def initialize_arguments(arg_parser: ArgumentParser) -> Namespace:
+    """
+    Parse command-line-arguments
+    """
+
+    arg_namespace = arg_parser.parse_args()
+
+    if arg_namespace.file[-4:] != ".asm":
+        arg_parser.print_usage()
+        sys.exit()
+
+    return arg_namespace
 
 
 if __name__ == "__main__":
-    file = initialize_arguments().file
-
-    print(file)
+    
+    arg_parser = initialize_argparser()
+    file = initialize_arguments(arg_parser).file
--- tests/test_hack_assembler.py
+++ tests/test_hack_assembler.py
@@ -1,17 +1,20 @@
 """
 Test methods for main hack_assembler module
 """
+import sys
+from io import StringIO
 from pytest import MonkeyPatch, raises
-from hack_assembler import initialize_arguments, Namespace
+from hack_assembler import initialize_arguments, initialize_argparser, Namespace
 
 monkeypatch = MonkeyPatch()
+arg_parser = initialize_argparser()
 
 def test_initialize_arguments():
     mock_filepath = "C:/File/Path.asm"
 
     monkeypatch.setattr("argparse.ArgumentParser.parse_args", lambda _: Namespace(file=mock_filepath))
 
-    args = initialize_arguments()
+    args = initialize_arguments(arg_parser)
 
     assert args.file == mock_filepath
 
@@ -20,6 +23,5 @@ def test_initialize_arguments_invalid_file():
 
     monkeypatch.setattr("argparse.ArgumentParser.parse_args", lambda _: Namespace(file=mock_filepath))
 
-    args = initialize_arguments()
-
-    assert False # TODO
+    with raises(SystemExit):
+        initialize_arguments(arg_parser)
