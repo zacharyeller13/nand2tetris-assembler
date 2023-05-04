599e521623f2fbbdfe19d76d00ab02ba75fefdff
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2023-05-04 18:48:10 -0500

    Add initial SymbolHandler setup

---
+++ symbol_handler.py
@@ -0,0 +1,9 @@
+"""
+Module to handle symbols in the .asm files
+"""
+
+from constants import PRE_DEFINED_SYMBOLS, LABEL_START, LABEL_END, VAR_START
+
+class SymbolHandler:
+    def __init__(self) -> None:
+        pass
