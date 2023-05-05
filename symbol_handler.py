e2e3330ff4e5c674f2dabdea02daf3cf35accf25
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2023-05-04 22:45:49 -0500

    Add some methods for handling all symbols and labels

--- symbol_handler.py
+++ symbol_handler.py
@@ -4,6 +4,43 @@ Module to handle symbols in the .asm files
 
 from constants import PRE_DEFINED_SYMBOLS, LABEL_START, LABEL_END, VAR_START
 
+
 class SymbolHandler:
+    """
+    Class that maintains the Symbol table and labels for a .asm file.
+
+    Attributes:
+        `symbol_table` (dict[str, int]): The symbol table that contains `PRE_DEFINED_SYMBOLS` as well as any
+            symbols encountered in the .asm file. Pre-defined symbols and @var symbols reference a memory address.
+            (Labels) reference the next line number in the file.
+        `next_address` int: Starting at 16, represents the memory address to be assigned to the next
+            encountered variable.
+    """
+
     def __init__(self) -> None:
-        pass
+        self.symbol_table: dict[str, int] = PRE_DEFINED_SYMBOLS
+        self._next_address: int = 16
+
+    def handle_symbol(self, symbol: str) -> None:
+        """
+        Handle a supplied symbol by adding it to the symbol table if it does not already exist there
+
+        Args:
+            `symbol` (str): The symbol to be checked and added to the `symbol_table` 
+                if it does not already exist
+        """
+
+        raise NotImplementedError
+
+    def _handle_label(self, label: str) -> None:
+        """
+        Labels are different from regular symbols in that they reference the next line-number 
+            rather than a memory address.  Handle them by maintaining the surrounding '()'
+            and making their value the next line-number (or even the next statement?).
+            Add to `symbol_table`
+
+        Args:
+            `label` (str): The label to be handled 
+        """
+
+        raise NotImplementedError
